94c6f344f32a4e15c7df3870ddc9c687e4107a4c
Spoorthi M Naik <naikspoorthi6@gmail.com>
2026-09-14 18:24:15 +0530

    Change base path in vite.config.js
    Update base path for GitHub Pages hosting.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,4 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative asset paths for GitHub Pages hosting
-});
+  base: '/LUMORA/',
+})
